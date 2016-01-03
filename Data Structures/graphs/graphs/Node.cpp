0f3c4499668d76550532551709973de6a01be443
John Liao <js.liao08@gmail.com>
2016-01-02 22:36:30 -0500

    initial commit

---
+++ Data Structures/graphs/graphs/Node.cpp
@@ -0,0 +1,17 @@
+//
+//  Node.cpp
+//  stacks
+//
+//  Created by John Liao on 12/12/15.
+//  Copyright © 2015 John Liao. All rights reserved.
+//
+
+#include "Node.hpp"
+
+Node::Node(int val) {
+    this->val = val;
+}
+
+int Node::getVal() {
+    return val;
+}
