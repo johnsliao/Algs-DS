a4aaeab1a720c97c6e7f093784cbf635d34c89c5
John Liao <js.liao08@gmail.com>
2016-01-02 22:38:26 -0500

    initial commit

---
+++ Data Structures/graphs/graphs/Node.hpp
@@ -0,0 +1,29 @@
+#ifndef Node_hpp
+#define Node_hpp
+
+#include <stdio.h>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+struct Node {
+    char val;
+    typedef vector<Node*> adj_list;
+    adj_list adj;
+    
+    Node(char val) {
+        this->val=val;
+    }
+    char getVal() {
+        return val;
+    }
+    void printAdj() {
+        for (adj_list::iterator itr = adj.begin(); itr !=adj.end(); itr++) {
+            cout << (*itr)->getVal() << " ";
+        }
+        cout << endl;
+    }
+};
+
+#endif /* Node_hpp */
